89920782aab597b4b62a2a72eea3915d91755872
randy <raandy.gc@gmail.com>
2026-03-14 23:49:28 -0300

    fix: configure Swagger server URL for production HTTPS

--- src/main/java/br/com/easypet/config/SwaggerConfig.java
+++ src/main/java/br/com/easypet/config/SwaggerConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${server.url:http://localhost:8080}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI openAPI() {
@@ -18,6 +25,7 @@ public class SwaggerConfig {
                         .title("EasyPet API")
                         .description("API para gerenciamento de pets e agendamentos")
                         .version("v1.0"))
+                .servers(List.of(new Server().url(serverUrl)))
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components()
                         .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
