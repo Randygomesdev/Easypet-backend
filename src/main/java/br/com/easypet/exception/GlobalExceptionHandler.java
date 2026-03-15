c9f27e0633adb7e5549539d04534f24919f49cfe
randy <raandy.gc@gmail.com>
2026-03-14 23:12:17 -0300

    feat: add Docker setup and deployment configuration

--- src/main/java/br/com/easypet/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/easypet/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package br.com.easypet.exception;
 
 import br.com.easypet.dto.response.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -13,6 +15,8 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+        private static final Logger log =  LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
@@ -46,7 +50,10 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        log.error("Erro interno: ", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.of(500, "Internal Server Error", "Erro interno do servidor"));
     }
+
+
 }
